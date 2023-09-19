41608112e298f1455cf5dcb90c69c6830fc792bc
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-09-19 02:51:51 -0700

    Regular proto

--- ze_printf.c
+++ ze_printf.c
@@ -21,24 +21,24 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-			ze_putchar(format[i]);
+			_putchar(format[i]);
 
 		else if (format[i + 1] == 'c')
 		{
-			ze_putchar(va_arg(print, int));
+			_putchar(va_arg(print, int));
 			i++;
 		}
 
 		else if (format[i + 1] == 's')
 		{
-			ze_count = ze_puts(va_arg(print, char *));
+			ze_count = _puts(va_arg(print, char *));
 			i++;
 			count += (ze_count - 1);
 		}
 
 		else if (format[i + 1] == '%')
 		{
-			ze_putchar('%');
+			_putchar('%');
 		}
 		count++;
 
