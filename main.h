6b02e094bfaf3c6e24002e6cf1c699f0ab493dcc
Zoi <140983437+3eIlla@users.noreply.github.com>
2023-09-19 03:08:46 -0700

    remov all but 0

--- main.h
+++ main.h
@@ -43,54 +43,6 @@ int _puts(char *c);
 
 
 
-#define PARAMS_INIT {0}
-/* .  paras_t paras = PARAMS_INIT;  */
-/**
- * struct paras - struc out
-*/
-typedef struct paras
-{
-	unsigned int unsign			: 1;
-} paras_t;
 
-void init_paras(paras_t *paras, va_list print);
-/*/ THE MAIN SPECIFIER FUNCTION/*/
-/**
- * struct spcfr- struc out
- * @spcfr: strin
- * @func: *function
-*/
-typedef struct spcfr
-{
-	/* data */
-	char *spcfr; /*string / 1 char*/
-	int (*func)(va_list, paras_t *);
-} spcfr_t; /*chang char to specifier type*/
-
-/*display*/
-/* puts 1st brings whole string for putchar */
-/* then putchar pasifier each single char*/
-
-/*1*/
-/*/c .     %c .    */
-int print_char(va_list print, paras_t *paras);
-/*/s .     %s .  */
-int print_string(va_list print, paras_t *paras);
-/*/% .     %% .    */
-int Print_percent(va_list print, paras_t *paras);
-
-/*to call spicifieres*/
-int get_print_func(char *spc, va_list print, paras_t *paras);
-int (*get_specifier(char *spc))(va_list print, paras_t *paras);
-
-
-
-
-
-/*4* local buffer of 1024 chars in order to call write as little as possible.*/
-
-#define OUT_PUT_BUFF_SIZE 1024
-#define BUFF_FLUSH -1
-/* . _putchar(BUF_FLUSH); */
 
 #endif
