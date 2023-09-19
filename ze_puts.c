0d1a3c7469db7a2f1cebe10c846dd3de26e9a7a7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2023-09-18 18:40:21 -0700

    ze_puts added

---
+++ ze_puts.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * ze_puts - prints the character
+ * @c: Input string
+ *
+ * Return: Number of bytes/charcter
+ */
+int ze_puts(char *c)
+{
+	int count = 0;
+
+	if (c)
+	{
+		for (count = 0; c[count] != '\0'; count++)
+		{
+			ze_putchar(c[count]);
+		}
+	}
+	return (count);
+}
+
